3c3606d72e73c5d12c040f1fab2b8ed3573bda11
Sam Collard <swift@swifteagle.co.uk>
2017-04-04 11:50:03 +0100

    Update terrors.rb

--- example/terrors.rb
+++ example/terrors.rb
@@ -1,11 +1,21 @@
+So begins 'The Terrors of the Halls'
+
+Introduction 'Slowly your eyes creak open.'
+  Choose to seek: 'The Manor'
+
 Section 'The Manor' -
   'You arrive just in time to see the door swing shut before you.'
 
   Choose to 'weep openly', seek: 'Despair'
   Choose to 'look for an alternative means of egress', seek: 'The Kitchen'
 
+Section 'Despair' -
+  'You cry a bit. You feel better (maybe?). Let\'s get back to that manor.'
+
+  Choose to 'pull yourself together', seek: 'The Manor'
+  Choose to 'continue crying', seek: 'Despair'
+
 Section 'The Kitchen' -
   'The snacks are delicious.'
 
   Thus ends 'Terrors of the Halls'
-
