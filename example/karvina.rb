82e560d220543b0d04bc8619eb95b6afc0bad141
Sam Collard <swift@swifteagle.co.uk>
2017-04-06 21:14:14 +0100

    Add The Karvina Job

---
+++ example/karvina.rb
@@ -0,0 +1,123 @@
+So begins 'The Karvina Job'
+
+Introduction 'The year is 2031. You are Trent Hardcable, grizzled freelance spy. Your grizzliness is exceeded only by your hard-boileditude, and strangely your hard-boileditude is exceeded only by your grizzliness. You have been hired to infiltrate a research laboratory operated by the enigmatic Karvina Corporation, make your way through the facility and escape with your objective: a USB drive containing the only copy of Karvina\'s newest prototype version control software.'
+  Choose to seek: 'Front Door'
+  
+Section 'Front Door' -
+  'You are standing outside the research lab, a small, squat building on the edge of town. It\'s early evening, and the air is still. The area is deserted; you\'ve seen no pedestrians or hovercars for the last few blocks on your way here. The double doors at the front of the facility are made of hologlass, and as you peer inside you can see an uninhabited reception area. Looking to your left you can see an alley leading around the side of the building, and on the second floor far above you there is an open window.
+  What will you do?'
+    
+  Choose to 'Enter through the front door', seek: 'Lobby'
+  Choose to 'Check the alley', seek: 'Back Alley'
+  Choose to 'Climb up to the open window', seek: 'Scaling Exterior Wall'
+    
+Section 'Lobby' -
+  'Your handy nanotech lockpicks make short work of the door, and you step forward into a well-lit lobby area. The only sound is the background hum of the air conditioning. To your left you see an unmanned receptionist\'s desk, and at the far end of the room is an elevator, doors open and waiting.
+  What will you do?'
+  
+  Choose to 'Search the receptionist\'s desk', seek: 'Desk'
+  Choose to 'Enter the elevator', seek: 'Elevator'
+  
+Section 'Desk' -
+  'The desk is spotless; a stapler and holepunch are arranged next to a desk tidy full of pens and pencils, all perfectly aligned. You run your finger across the surface to check for dust, and find none. As you waggle the mouse experimentally, the screensaver on the monitor disappears. Lucky you - the receptionist forgot to log out, and left the boss\'s appointments open!
+  Scanning through the scheduled appointments, you spot an entry from earlier this afternoon: "Version control demo setup for tomorrow, Meeting Room A". The prototype software must already be in place in the meeting room. You find a small floor plan of the building on the wall next to you, which tells you that Meeting Room A is on the top floor to the left of the elevator.
+  Armed with your newfound knowledge, you head for the elevator.'
+  
+  Choose to 'Take the elevator', seek: 'Elevator'
+  
+Section 'Elevator' -
+  'As you step into the elevator, the doors slide silently shut behind you. You see the familiar panel of buttons to the right of the doors.
+  Which button will you push?'
+  
+  Choose to 'Push the button labelled "2"', seek: 'Second Floor Corridor'
+  Choose to 'Push the button labelled "1"', seek: 'First Floor Corridor'
+  #Choose to 'Push the button labelled "B"', seek: 'Basement Corridor'
+  
+Section 'First Floor Corridor' -
+  'The elevator slides to a gentle stop, and you step out into a corridor that stretches away in both directions. Before you can properly get your bearings, you hear a high-pitched electronic whine. From down a nearby branch of the corridor, an inhuman, metallic voice drones "investigating disturbance" and you hear dull, thudding footsteps slowly approach. It can only be one of Karvina\'s infamous android security guards. You know them well; they are implacable, almost impossible to stop, and perfectly prepared to shoot first and ask questions later, or ask no questions at all.
+  What will you do?'
+  
+  Choose to 'Hide', seek: 'Behind a potted shrub'
+  Choose to 'Draw your sidearm, wait for the guard to appear and shoot to kill', seek: 'Gunfight'
+  Choose to 'Run for the stairs', seek: 'Stairwell, on the run'
+  
+Section 'Behind a potted shrub' -
+  'You quickly sit down and hug your knees to your chest behind the only cover within reach: a large decorative potted plant. You hold your breath as the android strides rhythmically past your hiding place. As it continues down the corridor, apparently having missed you, you breathe a sigh of relief.
+  Poor move. As the android turns, you suddenly recall that they have excellent hearing. From this side of the potted plant, you have no cover at all, and before you can stand up to run the android draws a revolver and fires. The bullet strikes you in the chest and you collapse to the ground. The last thing you see is the android walking slowly back towards you to finish you off.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Gunfight' -
+  'You draw your trusty Xylo 9mm, take careful aim at head height at the corner of the corridor and wait. The empty, metallic footsteps draw gradually closer. You hold your breath.
+  The android appears around the corner and turns towards you. You see it reach for a revolver at its belt. Wasting no time, you fire, the gunshot ringing down the corridor. You strike the android square in the face and it stumbles backwards, but does not fall. You fire twice more, striking it in the chest and then the head again, but it shrugs off both shots and draws its own sidearm. You suddenly remember that Karvina\'s android designs all incorporate reinforced armour from head to toe, just for this sort of scenario. Before you can fire again, the android aims with surgical precision and pulls the trigger, and the shot strikes you in the torso. You drop your weapon and fall to your knees. You watch, helpless, as the android strides slowly forward until it stands right in front of you, and aims at your temple. The last thing you see is the android\'s face and its cold, dead eyes at the other end of the barrel.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Stairwell, on the run' -
+  'You head for the end of the corridor at a dead sprint, not daring to look back. About ten feet from the door to the stairwell, you hear the soulless metallic voice shout "halt!", and a gunshot ricochets off the wall to your left. You run into the door, but it doesn\'t move an inch - the android must have remotely locked it!
+  Right as you turn to face the android, two more shots slam into your chest and you slump to the floor, your back against the locked door. The last thing you see is the android taking aim for one more shot.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Second Floor Corridor' -
+  'You step forward into the corridor. The lights are off, and the lack of windows leaves the area shrouded in gloom. The door closes behind you, and you are plunged into pitch darkness.
+  Which way will you go?'
+  
+  Choose to 'Go left', seek: 'Meeting Room A'
+  Choose to 'Go right', seek: 'Meeting Room B'
+  
+Section 'Back Alley' -
+  'You stride confidently around the corner into the alley, one hand on your trusty handgun, only to find it deserted. The floor is a strata of ancient newspapers and filth, and you see a rat scurry out into the street from behind some dustbins, disturbed by your presence. At the very end of the alley you spot an open ground-level ventilation window - the perfect infiltration point.
+  It\'s a tight squeeze, but you can just about fit through. You drop down to the floor of the room to find yourself in an employee bathroom, tiled in a hideous peachy-purple colour. The walls, floors and cubicles are spotless; you expect they\'re cleaned by androids. The only light comes from the window you used to enter, and the beam of dying sunlight falls on the door to the rest of the facility. You try the handle - locked. Worse, there\'s no keyhole. It must be electronically locked; your handy nanotech lockpicks are useless here.
+  You knock the access point covering off the wall with your elbow. With the last of the daylight from the window you see three wires leading into the door; red, yellow and blue. You squeeze your laser wirecutter in preparation.
+  Which wire will you cut?'
+  
+  Choose to 'Cut the red wire', seek: 'Electronics for Dummies'
+  Choose to 'Cut the yellow wire', seek: 'Basement Corridor'
+  Choose to 'Cut the blue wire', seek: 'Second Time Lucky'
+  
+Section 'Electronics for Dummies' -
+  'You slice effortlessly through the wire. Almost immediately, you hear alarms and sirens begin ringing throughout the complex; you\'ve been rumbled.
+  Before long, one of Karvina\'s infamous android security guards arrives, unlocks the door from the outside and steps in to apprehend you. If it had human hands, it would have cracked its knuckles before laying into you. You put up a good fight, but you are no match for an android. The last thing you see after your beating is the android\'s stainless steel boot coming down to crush your skull.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Basement Corridor' -
+  'You slice effortlessly through the wire. You hear the lock disengage, and as you push the door it swings gently open.
+  You step out into the corridor, finding it deserted. This must be the maintenance level; from end to end, the corridor is dotted with storage crates, racks of tools as well as spare ceiling tiles, squares of carpet and other fittings. Down the corridor to your left, past a pallet of water cooler bottles, there is an elevator; to your right, a short flight of stairs ending in a door.
+  What will you do?'
+  
+  Choose to 'Press the button to call the elevator', seek: 'Elevator'
+  Choose to 'Ascend the stairs', seek: 'Lobby'
+  
+Section 'Second Time Lucky' -
+  'You slice effortlessly through the wire. Absolutely nothing happens.
+  Which wire will you cut next?'
+  
+  Choose to 'Cut the red wire', seek: 'Basement Corridor'
+  Choose to 'Cut the yellow wire', seek: 'Flash of Brilliance'
+  
+Section 'Flash of Brilliance' -
+  'You slice effortlessly through the wire. The last thing you see is the blinding flash of the panel exploding, launching multiple shards of jagged metal into your eyes and face. You are left clawing at your bloodied face in agony, and before long you lose consciousness. It takes you most of the night to bleed out.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Scaling Exterior Wall' -
+  'Your augmented wrists and ankles make climbing sheer surfaces a breeze. You inch carefully up the wall until you reach the sill of the first floor window, where you can take a break and readjust yourself. As you raise your head above the sill, you come eye to beady eye with a nesting pigeon. It coos menacingly, and turns its head the other way to reveal a cybernetic eye on the other side - it must be a sentry! Before you can react, it pecks you in the face.
+  Losing your grip on the windowsill, you fall to ground level and land awkwardly, feeling your leg bend in a way it shouldn\'t. The last thing you see is a huge flock of pigeons, each with one eye glowing an artificial red, descending upon you.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Meeting Room A' -
+  'You step from the dark of the corridor into a dim meeting room. With the last dregs of daylight shining through the windows you see a large table and some twenty or thirty chairs in the centre of the room, and in the far corner a desk with a laptop. A darkened, inactive projector is fixed to the ceiling, pointed at the far wall.
+  You stride the length of the room to the laptop, and find your prize; the USB drive with the prototype version control software is plugged into the side of the machine, and is helpfully labelled "PROTOTYPE VERSION CONTROL SOFTWARE".
+  With the drive safe in your pocket, you retrace your steps through the building, and before long you find yourself back outside. By now, the daylight is totally gone; you take a breath of cool, crisp night air and breathe a sigh of relief. Your employers will pay you well for your work this evening, and also you didn\'t die, which is always nice. You begin a leisurely stroll down the street, and vanish into the night.'
+  
+  Thus ends 'The Karvina Job'
+  
+Section 'Meeting Room B' -
+  'You step from the dark of the corridor into a dim meeting room. A projector mounted to the ceiling beams the blue square of television tuned to a dead channel onto the far wall; by the dim light you see a large table in the centre of the room, with chairs stacked on top of it, and in the far corner a desk with a closed laptop.
+  You begin to walk the length of the room to the desk, planning to check the laptop for the treasured USB drive that you seek, but you end up taking only a few steps before tripping on something unseen in the ultramarine gloom and crashing to the ground hard. As you pick yourself up, a tinny, inhuman voice at ground level cries "unidentified lifeform detected! INTRUDER ALERT!" Peering hard at the floor, you see the culprit; a vacuum cleaner roomba, now flipped upside down and unable to right itself, its wheels whirring futilely.
+  You run back to the door, but right before you reach it you hear the dull thunk of the lock engaging. You are left with no option but to wait for the facility\'s security forces to find you; before long, a trio of Karvina\'s infamous android guards arrive, unlock the door and seize you. Without saying anything, they drag you towards the large windows, their grip remaining strong against your attempts to struggle. One android opens the window while the others lift you by the arms and legs. The last thing you see is the concrete rushing up to meet you.'
+  
+  Thus ends 'The Karvina Job'
